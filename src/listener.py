171c6938082249da62492d51e1a162cfc0f73697
maxj723 <46984489+maxj723@users.noreply.github.com>
2025-10-06 10:19:50 -0400

    update

--- src/listener.py
+++ src/listener.py
@@ -22,21 +22,22 @@ class MeshtasticListener:
             ts = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
 
             try:
-                # data = json.loads(msg)
-                # if isinstance(data, dict):
-                #     "timestamp": ts,
-                #     "node_id": node_id,
-                #     "message": msg
-                #     self.queue.put(data)
-                #     print(f"[{ts}] Received from {node_id}: {data}")
-
-                data = {
+                msg = msg.strip()
+                data = json.loads(msg)
+                if isinstance(data, dict):
                     "timestamp": ts,
                     "node_id": node_id,
                     "message": msg
-                }
-                self.queue.put(data)
-                print(f"[{ts}] From {node_id}: {msg}")
+                    self.queue.put(data)
+                    print(f"[{ts}] Received from {node_id}: {data}")
+
+                # data = {
+                #     "timestamp": ts,
+                #     "node_id": node_id,
+                #     "message": msg
+                # }
+                # self.queue.put(data)
+                # print(f"[{ts}] From {node_id}: {msg}")
 
             except json.JSONDecodeError:
                 print(f"Ignored non-JSON from {node_id}: {msg}")
